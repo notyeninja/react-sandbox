8eb605264895cde0048d6fe36921411d80872ef9
Milind Wankhede <notyeninja@gmail.com>
2024-07-19 16:37:51 -0700

    hooks - added dropdown and data bind animals

--- src/SearchPets.jsx
+++ src/SearchPets.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
+const ANIMALS = ["bird", "dog", "rabbit"];
+
 const SearchPet = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
   return (
     <div className="container">
       <form action="">
@@ -8,18 +14,25 @@ const SearchPet = () => {
         <input
           id="location"
           type="text"
-          value="location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
           className="form-control"
         />
         <label htmlFor="animal" className="form-label">
           Animal
         </label>
-        <input
-          id="animal"
-          type="text"
-          value="Animal"
+        <select
           className="form-control"
-        />
+          name="animal"
+          id="animal"
+          value={animal}
+          onChange={(e) => setAnimal(e.target.value)}
+        >
+          <option />
+          {ANIMALS.map((animal) => (
+            <option key={animal}>{animal}</option>
+          ))}
+        </select>
         <label htmlFor="breed" className="form-label">
           Breed
         </label>
