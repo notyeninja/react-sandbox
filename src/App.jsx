e85381973674db969ddef4902bc0cee36835754b
Milind Wankhede <notyeninja@gmail.com>
2024-07-19 00:00:37 -0700

    added few more plugins help eslint and converted the app to use jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(
-      Pet,
-      {
-        name: "Bella",
-        breed: "Labrador Retriever",
-        animal: "dog",
-      },
-      null
-    ),
-    React.createElement(
-      Pet,
-      {
-        name: "Whiskers",
-        breed: "Siamese",
-        animal: "cat",
-      },
-      null
-    ),
-    React.createElement(
-      Pet,
-      {
-        name: "Tweety",
-        breed: "Budgerigar",
-        animal: "parrot",
-      },
-      null
-    ),
-    React.createElement(
-      Pet,
-      {
-        name: "Mittens",
-        breed: "Maine Coon",
-        animal: "cat",
-      },
-      null
-    ),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Mittens" breed="Maine Coon" animal="cat" />
+      <Pet name="Tweety" breed="Budgerigar" animal="parrot" />
+      <Pet name="Bella" breed="Labrador Retriever" animal="dog" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
-const appRoot = ReactDOM.createRoot(container);
-appRoot.render(React.createElement(App));
+const appRoot = createRoot(container);
+appRoot.render(<App />);
